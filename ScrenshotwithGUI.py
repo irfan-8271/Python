d5cd98c2c0f75132b6f65e3798cda701f2163ab7
Md Irfan <mdi720408@gmail.com>
2026-02-10 16:06:24 +0530

    fix

--- ScrenshotwithGUI.py
+++ ScrenshotwithGUI.py
@@ -1,13 +1,18 @@
 import pyautogui
 import time
+import tkinter as gui
 
 def screenshot():
     name = input("You will have 5 seconds to bring the desired interface on screen after typing \n Enter the name to save with \t")
-    name = name+ str(time.time())+".jpg"
+    name = name+ (str)time.time()+".jpg"
     loc = f'D:/Programs/PYTHON/{name}'
     time.sleep(5)
     img = pyautogui.screenshot()
-    img.save(loc)
+    img.save(name)
     img.show()
 
-screenshot()
+root = gui.Tk()
+frame = gui. Frame(root)
+frame.pack ()
+button = gui. Button (frame, text= "Take ScreenShot", command=screenshot)
+button.pack(side=gui. LEFT)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,11 +3,9 @@ import time
 
 def screenshot():
     name = input("You will have 5 seconds to bring the desired interface on screen after typing \n Enter the name to save with \t")
-    name = name+ str(time.time())+".jpg"
+    name = name+ (str)time.time()+".jpg"
     loc = f'D:/Programs/PYTHON/{name}'
     time.sleep(5)
     img = pyautogui.screenshot()
-    img.save(loc)
+    img.save(name)
     img.show()
-
-screenshot()
